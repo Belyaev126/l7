57bd24bb1b45ab3f981e078e0732b493586d0730
Belyaev126 <70748554+Belyaev126@users.noreply.github.com>
2020-11-13 10:38:38 +0300

    Update ind_2.py

--- ind_2.py
+++ ind_2.py
@@ -27,8 +27,6 @@ if __name__ == '__main__':
     for i, item in enumerate(b):
         if item < a_min:
             i_min, a_min = i, item
-        if item >= a_max:
-            i_max, a_max = i, item
     a_new = a[i_min:i_max+1]
     res = 1
     for j in a_new:
